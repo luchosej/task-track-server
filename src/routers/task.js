69b40c4aef86eedc631e672d793997b455a30f36
lsejudo <luciano.sejudo@gmail.com>
2020-05-14 23:13:11 -0300

    update task endpoint

--- src/routers/task.js
+++ src/routers/task.js
@@ -48,16 +48,15 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-      useFindAndModify: false
-    })
+    const task = await Task.findById(req.params.id)
 
     if (!task)
       return res.status(404).send()
-    res.send(task)
     
+    updates.forEach(update => task[update] = req.body[update])
+    
+    await task.save()
+    res.send(task)
   } catch (e) {
     res.status(500).send(e)
   }
