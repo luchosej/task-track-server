7793a7c752db49495a9f5f4bd5f32bb22b2ed000
lsejudo <luciano.sejudo@gmail.com>
2020-05-17 01:26:58 -0300

    link users to tasks

--- src/routers/user.js
+++ src/routers/user.js
@@ -9,7 +9,7 @@ router.post('/users', async (req, res) => {
 
   try {
     await user.save()
-    res.status(201).send({ user, token })
+    res.status(201).send({ user })
   } catch (e) {
     res.status(400).send(e)
   }
