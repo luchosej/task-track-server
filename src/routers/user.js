13dbcd2c40f2826057166d98eaaabb427f1276ca
lsejudo <luciano.sejudo@gmail.com>
2020-05-19 00:38:37 -0300

    link users to tasks

--- src/routers/user.js
+++ src/routers/user.js
@@ -102,8 +102,9 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
   try {
     await req.user.remove()
-    res.send(user)
+    res.send(req.user)
   } catch (e) {
+    console.log(e)
     res.status(500).send(e)
   }
 })
