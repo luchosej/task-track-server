e82dc6961e846cc811a85dadfb67ac487f210b59
lsejudo <luciano.sejudo@gmail.com>
2020-07-11 18:28:09 -0300

    set enviroment variables

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('./db/mongoose')
 const routers = require('./routers')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(routers)
